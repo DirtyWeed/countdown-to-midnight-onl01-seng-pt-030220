dae80687a5088dda42508a55c5a50d90c0dd48de
Dylan Williamson <dirtyassweed@icloud.com>
2020-03-08 02:00:37 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,9 @@
 def countdown(seconds)
   while seconds > 0
-  puts "#{seconds} SECOND("
+  puts "#{seconds} SECOND(S)!"
+  seconds -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep
